7ec24fdde1fcb1bfe1694176aad3ed264ac695a7
J <J@J.J>
2017-03-26 21:17:04 +0300

    Revert "Cwap!"
    This reverts commit 20a3d2e184cb34db42bf07478e591d6325278714.

--- DiscordCharacters.py
+++ DiscordCharacters.py
@@ -160,12 +160,7 @@ class WoDCharacter:
             stats = stats.split('+')
         total = 0
         for stat in stats:
-            try:
-                stuff = self.get_numeric_stat(stat)
-                total += stuff
-            except Exception as e:
-                print(stuff + str(e))
-                return "Some error occurred!"
+            total += self.get_numeric_stat(stat)
         return total
     def create_resource(self,rsrc):
         self.resources[rsrc.capitalize()] = [1,1]
